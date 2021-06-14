56085eb227cd3e36157882f0dbc0aa1bcc915c9b
Nakkeeran Annamalai <Nakkeeran.Annamalai@zendesk.com>
2021-06-14 10:41:25 -0400

    Adding Header to request

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 Open API Validator to validates request and response against Swagger 2 and Open API contracts 
 
 ```bash
-Usage: oas-validator [-d=<requestBody>] -s=<schema> [-X=<method>]
+Usage: oas-validator [-hV] [-d=<requestBody>] -s=<schema> [-X=<method>]
                      [-H=<headers>]... <uri>
-      <uri>
-  -d, --data=<requestBody>
-  -H, --headers=<headers>
-  -s, --schema=<schema>
-  -X, --method=<method>
+      <uri>                  API Endpoint URL
+  -d, --data=<requestBody>   HTTP Request Body
+  -h, --help
+  -H, --headers              Http Headers=<headers>
+  -s, --schema=<schema>      Open API Schema URL or File
+  -V, --version
+  -X, --method=<method>      HTTP Method
 ```
+
+```bash
+java -jar open-api-validator.jar --schema https://petstore3.swagger.io/api/v3/openapi.json --method GET --header 'API-TOKEN:asdfasdfasdfasdfasdf' https://petstore3.swagger.io/api/v3/store/order/1234
+```
+
+####Build Instruction:
+- Java Version: Java 11
+
+```bash
+Mac/Unix: ./gradlew jar
+Windows: gradlew.bat jar
+```
+
--- src/main/java/com/naqiran/oas/validator/command/OASValidatorCommand.java
+++ src/main/java/com/naqiran/oas/validator/command/OASValidatorCommand.java
@@ -1,10 +1,12 @@
 package com.naqiran.oas.validator.command;
 
 import com.naqiran.oas.validator.OASValidator;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
+import javax.annotation.Nonnull;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.util.HashMap;
@@ -24,7 +26,7 @@ public class OASValidatorCommand implements Callable<String> {
     @Parameters(description = "API Endpoint URL")
     private URI uri;
 
-    @Option(names = {"-X", "--method"}, defaultValue = "GET")
+    @Option(names = {"-X", "--method"}, description = "HTTP Method", defaultValue = "GET")
     private String method;
 
     @Option(names = {"-d", "--data"}, defaultValue = "", description = "HTTP Request Body")
@@ -36,28 +38,41 @@ public class OASValidatorCommand implements Callable<String> {
     @Option(names = {"-s", "--schema"}, description = "Open API Schema URL or File", required = true)
     private String schema;
 
+    @Option(names = {"-h", "--help"}, usageHelp = true)
+    private boolean help;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true)
+    private boolean versionHelp;
+
     @Override
     public String call() {
         var builder = HttpRequest.newBuilder()
                 .uri(uri)
                 .method(method, HttpRequest.BodyPublishers.ofString(requestBody));
         for (var entry : getHeaders().entrySet()) {
-            builder.headers(entry.getKey(), entry.getValue());
+            builder.headers(entry.getKey(), entry.getValue().get(0));
         }
         var messages = OASValidator.builder().withSchema(schema).build().validate(builder.build());
         messages.getMessages().forEach(message -> LOGGER.info(message.toString()));
         return "success";
     }
 
-    public Map<String, String> getHeaders() {
-        final Map<String, String> headers = new HashMap<>();
-        return headers;
+    @Nonnull
+    private Map<String, List<String>> getHeaders() {
+        final Map<String, List<String>> headerMap = new HashMap<>();
+        headerMap.put("Content-Type", List.of("application/json"));
+        headerMap.put("Accept", List.of("application/json"));
+        if (headers != null) {
+            for (final var header: headers) {
+                final var headerKv = StringUtils.removeEnd(StringUtils.removeStart(header, "'"), "'").split(":");
+                headerMap.put(headerKv[0].trim(), List.of(headerKv[1].trim()));
+            }
+        }
+        return headerMap;
     }
 
     public static void main(final String[] args) {
-        String[] args1 = {"--schema=https://petstore3.swagger.io/api/v3/openapi.json", "https://petstore3.swagger.io/api/v3/store/order/1234", "--method=GET"};
+        String[] args1 = {};
         System.exit(new CommandLine(OASValidatorCommand.class).execute(args1));
-
-        //System.exit(new CommandLine(OASValidatorCommand.class).execute(args));
     }
 }
